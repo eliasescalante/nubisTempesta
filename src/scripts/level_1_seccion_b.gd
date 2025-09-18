658d2310aa2d2dcee5bc292cf1ff42d516d156c9
eliasescalante <eliasw.escalante@gmail.com>
2025-09-18 20:09:38 -0300

    cambios de version en godot 4.5

--- src/scripts/level_1_seccion_b.gd
+++ src/scripts/level_1_seccion_b.gd
@@ -3,8 +3,14 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var player = $Player
-	var spawn_point = $Portal1a2/Marker2D
 	if GameState.portal == 2:
+		var spawn_point = $Portal1a2/Marker2D
+		player.global_position = spawn_point.global_position
+		GameState.portal = 0
+		print("valor de portal ahora")
+		print(GameState.portal)
+	elif GameState.portal == 3:
+		var spawn_point = $Portal2a3/Marker2D
 		player.global_position = spawn_point.global_position
 		GameState.portal = 0
 		print("valor de portal ahora")
@@ -15,18 +21,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_portal_1a_2_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GameState.portal = 1
 		print(GameState.portal)
-		call_deferred("_change_to_loader")
-		
-func _change_to_loader():
-	get_tree().change_scene_to_file("res://src/scenes/levels/loader_2_1.tscn")
+		call_deferred("res://src/scenes/levels/loader_2_1.tscn")
 
 func _on_portal_2a_3_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		GameState.portal = 3
+		print(GameState.portal)
 		get_tree().change_scene_to_file("res://src/scenes/levels/loader_2_3.tscn")
-	pass # Replace with function body.
