f1aed03ac6b635e85370fe82023c7a5b218524ee
Martín Ochoa <mmochoa@gmail.com>
2025-10-06 22:13:13 -0300

    agrega elementos del HUD y los acomoda según la interfaz agrega animación PLD_icon icons DASH y DOBLE JUMP
    deshabilita el sistema de inventario de 4 slots.

--- src/scripts/ui/hud_nivel.gd
+++ src/scripts/ui/hud_nivel.gd
@@ -1,28 +1,43 @@
 extends CanvasLayer
 
-@onready var inventario_slots = [
-	$MarginContainer/HBoxContainer/TextureRect,
-	$MarginContainer/HBoxContainer/TextureRect2,
-	$MarginContainer/HBoxContainer/TextureRect3,
-	$MarginContainer/HBoxContainer/TextureRect4,
-]
-@onready var puntos_label = $MarginContainer/Label
+@onready var nivel_label: Label = %level_label
+@onready var zona_label: Label = %zone_label
+
+@onready var doble_jump_texture_rect: TextureRect = %dobleJump_TextureRect
+@onready var dash_texture_rect: TextureRect = %dash_TextureRect
+
+@onready var pld_icon_animated_sprite_2d: AnimatedSprite2D = %pldIcon_AnimatedSprite2D
+@onready var puntos_label: Label = %pldPoints_label
+
+@onready var object_used_texture_rect: TextureRect = %objectUsed_TextureRect
+@onready var object_quest_texture_rect: TextureRect = %objectQuest_TextureRect
+@onready var object_pass_texture_rect: TextureRect = %objectPass_TextureRect
+
+
 
 var puntos: int = 0
 var inventario: Array = [null, null, null, null] # Guarda los items
 
+func _ready() -> void:
+	pld_icon_animated_sprite_2d.play("default")
+	
 func actualizar_puntos(valor: int):
+	print("actualizar_puntos"+str(valor))
 	puntos = valor
-	puntos_label.text = "Puntos: %d" % puntos
+	puntos_label.text = "%d" % puntos
 
 func agregar_item(item_texture: Texture2D):
-	for i in range(inventario.size()):
-		if inventario[i] == null:
-			inventario[i] = item_texture
-			inventario_slots[i].texture = item_texture
-			return
+	
+	#for i in range(inventario.size()):
+	#	if inventario[i] == null:
+	#		inventario[i] = item_texture
+	#		inventario_slots[i].texture = item_texture
+	#		return
+	
+	return
 
 func quitar_item(index: int):
-	if inventario[index] != null:
-		inventario[index] = null
-		inventario_slots[index].texture = null
+	#if inventario[index] != null:
+	#	inventario[index] = null
+	#	inventario_slots[index].texture = null
+	return
