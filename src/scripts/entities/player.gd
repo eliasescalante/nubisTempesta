7ab98270efd70647da9ea04399414c7a13007d96
Martín Ochoa <mmochoa@gmail.com>
2025-10-20 23:35:34 -0300

    agrega testeo de animacino MORIR, CAER, IDLE agrega provisorio tecla trigger [H]ablar y [M]orir para disparar animaciones renombra animacion 'moving' x 'caminar' implementa la animación CAER cuando desciende. implementa penalidad por caer en diferentes alturas y morir.

--- src/scripts/entities/player.gd
+++ src/scripts/entities/player.gd
@@ -3,23 +3,32 @@ class_name Player
 
 # --- Constantes base ---
 const SPEED_BASE = 300.0
-const JUMP_VELOCITY_BASE = -1024.0
+const JUMP_VELOCITY_BASE = -1000.0
 const DASH_SPEED = 600.0
 const DASH_DURATION = 0.2
+const MORTAL_VELOCITY_FALL = 1600.0
 
 # --- Estados ---
 var is_dashing := false
 var dash_timer := 0.0
 var can_double_jump := true
+var is_talking := false
+var is_dead := false
+var velocity_falling := 0.0
 
 # --- Variables PLD ---
 @export var pld: int = 337
 @export var pld_por_salto: int = 5
 @export var pld_por_doble_salto: int = 7
 @export var pld_por_dash: int = 10
-@export var pld_por_tiempo_quieto: int = 1
-@export var tiempo_quieto: float = 1.0
 
+var pld_por_caida: int = 0
+var pld_por_caida_factor: float = 75.0
+
+@export var pld_por_morir: int = 140
+
+@export var pld_por_tiempo_quieto: int = 1
+@export var tiempo_quieto: float = 2.0
 var tiempo_sin_mover: float = 0.0
 
 # --- Referencias ---
@@ -56,6 +65,7 @@ func get_pld_multiplier() -> float:
 
 # --- Physics Process ---
 func _physics_process(delta: float) -> void:
+	
 	var on_floor := is_on_floor()
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
@@ -65,8 +75,10 @@ func _physics_process(delta: float) -> void:
 	var JUMP_VELOCITY = JUMP_VELOCITY_BASE * multiplier
 	
 	# --- Dash ---
+	# El dash reduce a la mitad la velocidad de caida
 	if Input.is_action_just_pressed("dash") and not is_dashing:
 		if pld >= pld_por_dash:
+			velocity_falling = velocity_falling / 2
 			start_dash(direction)
 			gastar_pld(pld_por_dash)
 	
@@ -76,8 +88,27 @@ func _physics_process(delta: float) -> void:
 			is_dashing = false
 
 	# --- Gravedad ---
+	# velocity_falling va registrando la velocidad de caida
+	# para calcular la penalidad en PLD por aterrizar.
 	if not on_floor and not is_dashing:
 		velocity += get_gravity() * delta
+		if velocity.y > velocity_falling:
+			velocity_falling = velocity.y
+
+	# --- Aterrizar
+	if velocity_falling > 0.0 and on_floor:
+		print("Aterrizar")
+		print("velociad de caida",str(velocity_falling))
+		# Determinar si se muere o no
+		if velocity_falling >= MORTAL_VELOCITY_FALL:
+			print('morir')
+			is_dead = true
+			gastar_pld(pld_por_morir)
+		else:
+			pld_por_caida = int ( velocity_falling / pld_por_caida_factor )
+			print('pld_por_caida', str(pld_por_caida))
+			gastar_pld(pld_por_caida)
+		velocity_falling = 0.0
 
 	# --- Salto / Doble salto ---
 	if Input.is_action_just_pressed("ui_accept"):
@@ -110,9 +141,23 @@ func _physics_process(delta: float) -> void:
 		if tiempo_sin_mover >= tiempo_quieto:
 			gastar_pld(pld_por_tiempo_quieto)
 			tiempo_sin_mover = 0
+			if is_dead == true:
+				print('dejar de hacerse la muerta')
+				is_dead = false
+			if is_talking == true:
+				print('dejar de hablar')
+				is_talking = false	
 	else:
 		tiempo_sin_mover = 0
 
+	if Input.is_action_just_pressed("morir") and on_floor:
+		print('morir')
+		is_dead = true
+
+	if Input.is_action_just_pressed("hablar") and on_floor:
+		print('hablar')
+		is_talking = true
+		
 	move_and_slide()
 	update_animation(on_floor, direction)
 
@@ -132,6 +177,13 @@ func play_anim(name : String) -> void:
 		animated_sprite_2d.play(name)
 
 func update_animation(on_floor : bool, direction : float) -> void:
+	
+	if is_dead:
+		play_anim("morir")
+		return
+	if is_talking:
+		play_anim("hablar")
+		return
 	if is_dashing:
 		play_anim("dash")
 	elif not on_floor:
@@ -141,9 +193,9 @@ func update_animation(on_floor : bool, direction : float) -> void:
 			else:
 				play_anim("jump")
 		else:
-			play_anim("jump")
+			play_anim("caer")
 	elif on_floor and abs(velocity.x) > 10:
-		play_anim("moving")
+		play_anim("caminar")
 	else:
 		play_anim("idle")
 
