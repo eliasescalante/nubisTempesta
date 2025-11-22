11d9fd024b96e8c9e35f2a0a27857e09283764ac
eliasescalante <eliasw.escalante@gmail.com>
2025-11-22 18:08:16 -0300

    button -> link credits

--- src/scripts/creditos.gd
+++ src/scripts/creditos.gd
@@ -14,3 +14,8 @@ func _process(delta: float) -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/main.tscn")
 	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	OS.shell_open("https://nubis.ar/")
+	pass # Replace with function body.
