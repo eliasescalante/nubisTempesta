f96d2ce486a326ef7b32363b31b9ae02b4b7ea1d
Martín Ochoa <mmochoa@gmail.com>
2025-11-24 23:36:51 -0300

    implementa cortina en main menu y creditos la quita de la escena 'main' y la mueve a 'main_menu'

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -1,14 +1,4 @@
 extends Node
 
-@onready var animacion = $cortina/AnimationPlayer
-@onready var cortina: CanvasLayer = $cortina
-
 func _ready() -> void:
 	AudioManager.play_intro()
-	animacion.play("entrada")
-	animacion.animation_finished.connect(_on_animacion_terminada)
-	
-func _on_animacion_terminada(anim_name: String) -> void:
-	print("Animacion cortina "+anim_name+" finalizada")
-	# Ocultamos la cortina para habilitar los botones
-	cortina.visible = false
--- src/scripts/ui/main_menu.gd
+++ src/scripts/ui/main_menu.gd
@@ -2,30 +2,48 @@ extends Control
 
 class_name MainMenu
 
+@onready var animacion = $cortina/AnimationPlayer
+@onready var cortina: CanvasLayer = $cortina
 
+var ir_a = ""
+
+func _ready() -> void:
+	animacion.play("entrada")
+	animacion.animation_finished.connect(_on_animacion_terminada)
+	
+func _on_animacion_terminada(anim_name: String) -> void:
+	print("Animacion cortina "+anim_name+" finalizada")
+	if anim_name == "entrada":
+		# Ocultamos la cortina para habilitar los botones
+		cortina.visible = false
+	if anim_name == "salida":
+		if ir_a == "juego":
+			AudioManager.get_node("ost/Introduccion").stop()
+			
+			GameState.portal = 0
+			GameState.loader = 0
+			GameState.text_loader = "NIVEL 1"
+			GameState.text_loader_subtitulo = "EL 'PLD' DE CADA DÍA"
+			GameState.image_loader_mini = "nivel_1"
+			get_tree().change_scene_to_file("res://src/scenes/levels/loader.tscn")
+			deactivate()
+		elif ir_a == "creditos":
+			get_tree().change_scene_to_file("res://src/scenes/levels/creditos.tscn")
+	
 func _on_play_button_pressed() -> void:
 	#LevelManager.load_level(3)
 	#get_tree().change_scene_to_file("res://src/scenes/levels/intro.tscn")
 	
+	cortina.visible = true
+	animacion.play("salida")
+	ir_a = "juego"
 	
-	AudioManager.get_node("ost/Introduccion").stop()
-	
-	GameState.portal = 0
-	GameState.loader = 0
-	GameState.text_loader = "NIVEL 1"
-	GameState.text_loader_subtitulo = "EL 'PLD' DE CADA DÍA"
-	GameState.image_loader_mini = "nivel_1"
-	get_tree().change_scene_to_file("res://src/scenes/levels/loader.tscn")
-	
-	deactivate()
-
-
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
 func deactivate() -> void:
-	hide()
+	#hide()
 	set_process(false)
 	set_process_unhandled_input(false)
 	set_process_input(false)
@@ -33,7 +51,7 @@ func deactivate() -> void:
 
 
 func activate() -> void:
-	show()
+	#show()
 	set_process(true)
 	set_process_unhandled_input(true)
 	set_process_input(true)
@@ -41,5 +59,6 @@ func activate() -> void:
 
 
 func _on_credits_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/levels/creditos.tscn")
-	pass # Replace with function body.
+	cortina.visible = true
+	animacion.play("salida")
+	ir_a = "creditos"
