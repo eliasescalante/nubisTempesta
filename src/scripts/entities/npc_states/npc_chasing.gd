66e2fc31cd506ed6f984f9509de18f97a4f09754
Martín Ochoa <mmochoa@gmail.com>
2025-11-30 14:40:10 -0300

    corrige en el State CHASING el movimiento ahora detecta si el RayCast está contacto con la plataforma

--- src/scripts/entities/npc_states/npc_chasing.gd
+++ src/scripts/entities/npc_states/npc_chasing.gd
@@ -5,6 +5,7 @@ class_name NpcChasing
 @export var move_speed : float = 200.0
 
 var player: CharacterBody2D
+var ray_cast_2d: RayCast2D
 
 # Al entrar, nos suscribimos a la señal de "player_lost"
 # del npc y capturamos el nodo principal del player
@@ -16,6 +17,7 @@ func enter():
 	# Buscamos al player por grupo
 	GameState.update_npc_property( npc, 'state', 'NpcChasing' )
 	player = get_tree().get_first_node_in_group("player")
+	ray_cast_2d = npc.ray_cast_2d
 
 # Al salir, desconectamos la señal
 func exit():
@@ -25,12 +27,16 @@ func exit():
 func physics_update(_delta: float):
 	if not player:
 		return
-	
-	# Diferencia entre 2 puntos en el plano. ESTO CAMBIARLO SOLO por COORD X
-	var direction = player.global_position - npc.global_position
-	direction.y = 0
-	npc.velocity = direction.normalized() * move_speed
-
+		
+	var direction
+	if ray_cast_2d.is_colliding():
+		# Diferencia entre 2 puntos en el plano. ESTO CAMBIARLO SOLO por COORD X
+		direction = player.global_position - npc.global_position
+		direction.y = 0
+		npc.velocity = direction.normalized() * move_speed
+	else:
+		direction = Vector2.ZERO
+		npc.velocity = direction
 
 # Callback cuando el npc detecta que el player se fue
 func _on_chase_player_lost():
