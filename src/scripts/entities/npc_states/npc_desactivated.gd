cfe54e18ebbc3663160aa21308e475a6e7ad1a95
Martín Ochoa <mmochoa@gmail.com>
2025-11-29 23:00:01 -0300

    agrega State NpcDesactivated

--- src/scripts/entities/npc_states/npc_desactivated.gd
+++ src/scripts/entities/npc_states/npc_desactivated.gd
@@ -1,1 +1,20 @@
-extends Node
+extends StateNPCs
+class_name NpcDesactivated
+
+@export var npc: CharacterBody2D # Apunta al nodo NpcEstorbo (2 parent levels)
+
+func enter():
+	print("STATE NpcDesactivated ENTER")
+	print("npc.name ", npc.name)
+	GameState.update_npc_data( npc, 'state', 'NpcDesactivated' )
+	GameState.update_npc_data( npc, 'quest', true )
+	npc.set_process_mode(PROCESS_MODE_DISABLED)
+	
+func exit():
+	pass
+		 
+func physics_update(_delta: float):
+	pass
+	
+func update(_delta: float):
+	pass
