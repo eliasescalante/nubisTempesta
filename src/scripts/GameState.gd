f56b3d931051c33938638cf46c37bc0c99301f62
Martín Ochoa <mmochoa@gmail.com>
2025-12-02 20:48:20 -0300

    agrega DialogManager base y renombra

--- src/scripts/GameState.gd
+++ src/scripts/GameState.gd
@@ -1,6 +1,7 @@
 extends Node
 
 # ------------------------------------------------------------------------------
+var tutorial = true
 
 # DATA PARA EL LOADER ENTRE ESCENAS
 var portal = 0 #1 es al nivel inicial, 2 al nivel 2 y el 3
